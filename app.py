e5cd5a18a358c1a481c493741a51f5fa5d248b5d
Vijay P <vijay.prahladan@gmail.com>
2024-01-11 20:28:18 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import boto3
+
+def list_s3_buckets():
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+
+    # Output the bucket names
+    print('Existing buckets:')
+    for bucket in response['Buckets']:
+        print(f'  {bucket["Name"]}')
+
+list_s3_buckets()
